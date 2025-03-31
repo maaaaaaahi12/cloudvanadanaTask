7da08160e41ba8b2d735a5c5e2ebc342cad743fb
unknown <maheshb1717@gmail.com>
2025-03-31 12:19:48 +0530

    completed the assisgnment

---
+++ javascripttask/index.js
@@ -0,0 +1,9 @@
+function checkisAnagram(str1,str2){
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    if(str1.length !== str2.length){
+        return false;
+    }
+    return str1.split('').sort().join('') === str2.split('').sort().join('')
+}
+console.log(checkisAnagram("MAhesh","mahesh"));
